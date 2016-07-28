3688e5189051a257896a12d320735b5929360fa4
Romain Durand <sirius2006@gmail.com>
2016-07-29 00:49:12 +0200

    simple isomorphism logic

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var fs = require("fs");
 var Client = require("./public/script");
 
 app.get("/page/:page?", function (req, res) {
-	var page = req.params.page || "/";
+	console.log("page/ req.params.page : " + req.params.page);
+	var page = req.params.page ? "/" + req.params.page : "/";
 	res.send(renderContent(page));
 });
 app.get("/", getPage);
@@ -16,18 +17,21 @@ app.listen(3000, function () {
 });
 
 function renderContent(path) {
-	if (path === "blog") {
-		return "<a href=\"/\">Home</a>";
-	}
-	return "Hello World!" + path;
+	var client = new Client();
+	console.log("renderContent path : " + path);
+	if (client.isValidPath(path)) {
+		if (path === "/blog") {
+			return "<a href=\"/\">Home</a>";
+		}
+		return "Hello World!" + path;
+	} else return "404";
 }
 
 function getPage(req, res) {
 	var client = new Client();
 	if (!client.isValidPath(req.url)) {
 		console.log("404 " + req.url);
-		res.status(404).send("not found");
-		return;
+		res.status(404);
 	}
 	fs.readFile("./public/index.html", "utf-8", function (err, data) {
 		res.setHeader("content-type", "text/html");
