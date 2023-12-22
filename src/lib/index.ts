e1895ffee1ac1390ccae26f2486ec12b1b2d740b
Romain Durand <romain.durand@radiofrance.com>
2023-12-22 05:25:12 +0100

    update post slug

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,13 @@
 // place files you want to import through the `$lib` alias in this folder.
 
-import type { Post } from "@prisma/client";
+import type { Post } from '@prisma/client';
 
 export function getSlug(post: Post) {
-  return `${post.title.replace(/\s+/g, '-').toLowerCase()}-${post.id}`;
+	const slug = `${post.title}-${post.id}`
+		.replace(/\s+/g, '-')
+		.normalize('NFD')
+		.replace(/[\u0300-\u036f]/g, '')
+		.toLowerCase()
+		.replace(/[^a-z0-9\s]/g, '-');
+	return slug;
 }
